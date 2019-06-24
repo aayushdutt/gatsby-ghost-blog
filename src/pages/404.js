99f8e4c39f2d0b2837bd693f257c8c792e2499d0
aayushdutt <aayushd100@gmail.com>
2019-06-24 18:42:44 +0530

    update meta

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,9 +6,9 @@ const NotFoundPage = () => (
     <Layout>
         <div className="container">
             <article className="content" style={{ textAlign: `center` }}>
-                <h1 className="content-title">Error 404</h1>
+                <h1 className="content-title">Oops! 404</h1>
                 <section className="content-body">
-                    Page not found, <Link to="/">return home</Link> to start over
+                    I couldn't find that page, <Link to="/">go back</Link>
                 </section>
             </article>
         </div>
