3043220b468a23a0bf46f212293e5d17ff82e80e
aayushdutt <aayushd100@gmail.com>
2019-07-14 17:27:32 +0530

    remove site navigation and move about to top

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -49,6 +49,7 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                                 { site.twitter && <a href={ twitterUrl } className="site-nav-item" target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/twitter.svg" alt="Twitter" /></a>}
                                 { site.facebook && <a href={ facebookUrl } className="site-nav-item" target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/facebook.svg" alt="Facebook" /></a>}
                                 <a className="site-nav-item" href={ `https://feedly.com/i/subscription/feed/${config.siteUrl}/rss/` } target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/rss.svg" alt="RSS Feed" /></a>
+                                <a className="site-nav-button" href="https://aayushdutt.com">About</a>
                             </div>
                         </div>
                         { isHome ?
@@ -57,15 +58,15 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                                 <p className="site-banner-desc">{site.description}</p>
                             </div> :
                             null}
-                        <nav className="site-nav">
+                        {/* <nav className="site-nav">
                             <div className="site-nav-left">
-                                {/* The navigation items as setup in Ghost */}
+                                The navigation items as setup in Ghost 
                                 <Navigation data={site.navigation} navClass="site-nav-item" />
                             </div>
                             <div className="site-nav-right">
                                 <a className="site-nav-button" href="https://aayushdutt.com">About</a>
                             </div>
-                        </nav>
+                        </nav> */}
                     </div>
                 </header>
 
